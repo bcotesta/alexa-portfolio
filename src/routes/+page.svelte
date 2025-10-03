2aca826f05ea1d791c058deff279787e275b6eac
bcotesta <bcotesta@gmail.com>
2025-10-03 19:30:45 -0400

    i'm TROLLING

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,4 @@
 <img src="/memes/sadcat.jpg" alt="White and gray cat with teary eyes and a downturned mouth sitting indoors, expressing a sad and forlorn emotion, with a blurred background suggesting a home environment" />
+<footer>
+    <p>COPYRIGHT © 2025 <a target="_blank" href="https://bcotesta.xyz">BRANDON COTESTA</a></p>
+</footer>
